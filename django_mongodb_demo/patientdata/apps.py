218c22724f7f7af3e9927cf2689eec1b749f3e9a
Jeffrey A. Clark <aclark@aclark.net>
2025-10-21 09:16:39 -0400

    Revert "Move qe/ to patientdata app"
    This reverts commit 7fca0d9041c980f23c73f727e19046800ad7e742.

--- django_mongodb_demo/patientdata/apps.py
+++ django_mongodb_demo/patientdata/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PatientDataConfig(AppConfig):
-    default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
+class PatientdataConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "patientdata"
